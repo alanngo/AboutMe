7b1b17db50edb48c7de02ed46243c8bea1c9f6e7
Alan <alan@remote.com>
2020-05-09 21:15:18 -0500

    aligned tools in center and added peripherals in PC builds

--- slideshow.js
+++ slideshow.js
@@ -1,28 +1,28 @@
 var i = 0;
-            var images = [];
-            var time = 5000;
-            var dir = "PC/"
-            
-            /*image list*/
-            images[0]=dir+"first build.jpg"
-            images[1]=dir+"front.jpg"
-            images[2]=dir+"back.jpg"
-            images[3]=dir+"setup0.jpg"
-            images[4]=dir+"setup2.jpg"
+var images = [];
+var time = 5000;
+var dir = "PC/"
 
-            function changeImg()
-            {
-                document.slide.src = images[i];
-                
-                if (i <images. length - 1)
-                {
-                    i++
-                }
-                else
-                {
-                    i =0;
-                }
-                setTimeout("changeImg()", time);
-            }
+/*image list*/
+images[0]=dir+"first build.jpg"
+images[1]=dir+"front.jpg"
+images[2]=dir+"back.jpg"
+images[3]=dir+"setup0.jpg"
+images[4]=dir+"setup2.jpg"
 
-            window.onload = changeImg;
+function changeImg()
+{
+    document.slide.src = images[i];
+    
+    if (i <images. length - 1)
+    {
+        i++
+    }
+    else
+    {
+        i =0;
+    }
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
