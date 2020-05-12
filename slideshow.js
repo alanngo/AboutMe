e1a0431a2014e23a343d5e8204f6d5e03f8fd94c
Alan <alan@remote.com>
2020-05-11 20:30:38 -0500

    fixed broken ut austin link

--- slideshow.js
+++ slideshow.js
@@ -14,14 +14,10 @@ function changeImg()
 {
     document.slide.src = images[i];
     
-    if (i <images. length - 1)
-    {
-        i++
-    }
-    else
-    {
-        i =0;
-    }
+    if (i <images. length - 1){i++}
+    
+    else{i =0;}
+    
     setTimeout("changeImg()", time);
 }
 
